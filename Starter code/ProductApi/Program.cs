7576f79de97865648c6dbe326e3b91cac2623e01
samirustill <gsaru008@edu.xamk.fi>
2026-04-02 12:26:30 +0300

    itsenäine osio

--- Starter code/ProductApi/Program.cs
+++ Starter code/ProductApi/Program.cs
@@ -10,6 +10,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
